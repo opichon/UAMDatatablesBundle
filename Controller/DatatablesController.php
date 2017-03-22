1584b3000ff789a430d662fabda7e2460cb4283c
Olivier Pichon <op@united-asian.com>
2017-03-22 14:57:04 +0545

    updated controllers

--- Controller/DatatablesController.php
+++ Controller/DatatablesController.php
@@ -6,13 +6,15 @@
  */
 namespace UAM\Bundle\DatatablesBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 /**
  * Provides convenience methods for implementing an index page listing database
  * records as entities using the dataTables jquery plugin with server-side data.
  *
  * @Template()
  */
-trait DatatablesController
+class DatatablesController extends Controller
 {
     use DatatablesControllerTrait;
 }
